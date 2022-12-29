d0f044481083356f3ebb84af8aa3c5f03f58ddce
Amril Azim <amrilazim@Amrils-MacBook-Pro.local>
2022-12-30 06:56:16 +0800

    add index method

--- app/Http/Controllers/Donor/DashboardController.php
+++ app/Http/Controllers/Donor/DashboardController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Donor;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Dashboard');
+    }
 }
