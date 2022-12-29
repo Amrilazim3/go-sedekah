6e3aa52871b8cb71a7179818abd0bead077b90e8
Amril Azim <amrilazim@Amrils-MacBook-Pro.local>
2022-12-30 06:42:38 +0800

    add user's role under inertia array

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,8 +36,14 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        $user = $request->user();
+
         return array_merge(parent::share($request), [
-            //
+            'inertia' => [
+                'user' => $user == null ? null : [
+                    'role' => $user->getRoleNames()
+                ]
+            ]
         ]);
     }
 }
