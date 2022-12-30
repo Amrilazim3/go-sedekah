38709fff36dea7cd6c3e1f015f921e845799e8ed
Amril Azim <amrilazim@Amrils-MacBook-Pro.local>
2022-12-31 07:34:57 +0800

    add footer

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -1,89 +1,89 @@
 <script setup></script>
 
 <template>
-    <footer class="bg-gray-50 text-center lg:text-left">
+    <footer class="bg-gray-900 text-center lg:text-left">
         <div class="container p-6">
             <div class="grid lg:grid-cols-4 md:grid-cols-2">
                 <div class="mb-6">
-                    <h5 class="uppercase font-bold mb-2.5 text-gray-800">
+                    <h5 class="uppercase font-bold mb-2.5 text-gray-300">
                         Donation agency 1
                     </h5>
 
                     <ul class="list-none mb-0">
                         <li>
-                            <a href="#!" class="text-gray-800">Link 1</a>
+                            <a href="#!" class="text-gray-300">Link 1</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-gray-800">Link 2</a>
+                            <a href="#!" class="text-gray-300">Link 2</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-gray-800">Link 3</a>
+                            <a href="#!" class="text-gray-300">Link 3</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-gray-800">Link 4</a>
+                            <a href="#!" class="text-gray-300">Link 4</a>
                         </li>
                     </ul>
                 </div>
 
                 <div class="mb-6">
-                    <h5 class="uppercase font-bold mb-2.5 text-gray-800">
+                    <h5 class="uppercase font-bold mb-2.5 text-gray-300">
                         Donation agency 2
                     </h5>
 
                     <ul class="list-none mb-0">
                         <li>
-                            <a href="#!" class="text-gray-800">Link 1</a>
+                            <a href="#!" class="text-gray-300">Link 1</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-gray-800">Link 2</a>
+                            <a href="#!" class="text-gray-300">Link 2</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-gray-800">Link 3</a>
+                            <a href="#!" class="text-gray-300">Link 3</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-gray-800">Link 4</a>
+                            <a href="#!" class="text-gray-300">Link 4</a>
                         </li>
                     </ul>
                 </div>
 
                 <div class="mb-6">
-                    <h5 class="uppercase font-bold mb-2.5 text-gray-800">
+                    <h5 class="uppercase font-bold mb-2.5 text-gray-300">
                         Donation agency 3
                     </h5>
 
                     <ul class="list-none mb-0">
                         <li>
-                            <a href="#!" class="text-gray-800">Link 1</a>
+                            <a href="#!" class="text-gray-300">Link 1</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-gray-800">Link 2</a>
+                            <a href="#!" class="text-gray-300">Link 2</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-gray-800">Link 3</a>
+                            <a href="#!" class="text-gray-300">Link 3</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-gray-800">Link 4</a>
+                            <a href="#!" class="text-gray-300">Link 4</a>
                         </li>
                     </ul>
                 </div>
 
                 <div class="mb-6">
-                    <h5 class="uppercase font-bold mb-2.5 text-gray-800">
+                    <h5 class="uppercase font-bold mb-2.5 text-gray-300">
                         Donation agency 4
                     </h5>
 
                     <ul class="list-none mb-0">
                         <li>
-                            <a href="#!" class="text-gray-800">Link 1</a>
+                            <a href="#!" class="text-gray-300">Link 1</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-gray-800">Link 2</a>
+                            <a href="#!" class="text-gray-300">Link 2</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-gray-800">Link 3</a>
+                            <a href="#!" class="text-gray-300">Link 3</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-gray-800">Link 4</a>
+                            <a href="#!" class="text-gray-300">Link 4</a>
                         </li>
                     </ul>
                 </div>
@@ -91,11 +91,11 @@
         </div>
 
         <div
-            class="bg-gray-200 text-gray-700 text-center p-4"
+            class="bg-gray-900 text-white border-t border-t-gray-50 text-center p-4"
         >
             © 2021 Copyright:
-            <a class="text-gray-800" href="https://tailwind-elements.com/"
-                >Tailwind Elements</a
+            <a class="text-indigo-500" href="https://go-sedekah.amrilazim.com"
+                >Go Sedekah</a
             >
         </div>
     </footer>
