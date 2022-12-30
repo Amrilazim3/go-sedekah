d2f6f8bfa61eb019c15bb4658d9ac5b85538a201
Amril Azim <amrilazim@Amrils-MacBook-Pro.local>
2022-12-30 09:34:39 +0800

    create footer components

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,102 @@
+<script setup></script>
+
+<template>
+    <footer class="bg-gray-50 text-center lg:text-left">
+        <div class="container p-6">
+            <div class="grid lg:grid-cols-4 md:grid-cols-2">
+                <div class="mb-6">
+                    <h5 class="uppercase font-bold mb-2.5 text-gray-800">
+                        Donation agency 1
+                    </h5>
+
+                    <ul class="list-none mb-0">
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="mb-6">
+                    <h5 class="uppercase font-bold mb-2.5 text-gray-800">
+                        Donation agency 2
+                    </h5>
+
+                    <ul class="list-none mb-0">
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="mb-6">
+                    <h5 class="uppercase font-bold mb-2.5 text-gray-800">
+                        Donation agency 3
+                    </h5>
+
+                    <ul class="list-none mb-0">
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="mb-6">
+                    <h5 class="uppercase font-bold mb-2.5 text-gray-800">
+                        Donation agency 4
+                    </h5>
+
+                    <ul class="list-none mb-0">
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-gray-800">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div
+            class="bg-gray-200 text-gray-700 text-center p-4"
+        >
+            © 2021 Copyright:
+            <a class="text-gray-800" href="https://tailwind-elements.com/"
+                >Tailwind Elements</a
+            >
+        </div>
+    </footer>
+</template>
