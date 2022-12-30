0eb253a4c7e48d4e252382b8b1395ab377a015ef
Amril Azim <amrilazim@Amrils-MacBook-Pro.local>
2022-12-30 10:58:17 +0800

    remove all dashboard route foreach user's role

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Donor\DashboardController as DonorDashboardController;
 use App\Http\Controllers\Needy\DashboardController as NeedyDashboardController;
 use App\Http\Controllers\WelcomeController;
@@ -27,31 +28,5 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function (Request $request) {
-        return redirect()->route($request->user()->getRoleNames()[0] . '.dashboard');
-    })->name('dashboard');
-
-    // admin routes
-    Route::prefix('admin')
-        ->middleware('role:admin')
-        ->name('admin.')
-        ->group(function () {
-            Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
-        });
-
-    // donor routes
-    Route::prefix('donor')
-        ->middleware('role:donor')
-        ->name('donor.')
-        ->group(function () {
-            Route::get('/dashboard', [DonorDashboardController::class, 'index'])->name('dashboard');
-        });
-
-    // needy routes
-    Route::prefix('needy')
-        ->middleware('role:needy')
-        ->name('needy.')
-        ->group(function () {
-            Route::get('/dashboard', [NeedyDashboardController::class, 'index'])->name('dashboard');
-        });
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
