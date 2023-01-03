3bba7b60101729bce722e54b97ac9be8a6a00af1
Amril Azim <amrilazim@Amrils-MacBook-Pro.local>
2023-01-03 23:53:22 +0800

    change color background

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -12,7 +12,7 @@ const props = defineProps({
     },
     contentClasses: {
         type: Array,
-        default: () => ['py-1', 'bg-white'],
+        default: () => ['py-1', 'bg-gray-900'],
     },
 });
 
@@ -65,7 +65,7 @@ const alignmentClasses = computed(() => {
         >
             <div
                 v-show="open"
-                class="absolute z-50 mt-2 rounded-md shadow-lg"
+                class="absolute z-50 mt-2 border border-gray-500 rounded-md shadow-lg"
                 :class="[widthClass, alignmentClasses]"
                 style="display: none;"
                 @click="open = false"
