dc86de60f67b6b8e81735ef666c2e0cfc1431a26
Amril Azim <amrilazim@Amrils-MacBook-Pro.local>
2023-01-04 08:11:23 +0800

    display action options by given type props

--- resources/js/Components/UsersDataTable.vue
+++ resources/js/Components/UsersDataTable.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { EllipsisVerticalIcon } from "@heroicons/vue/20/solid";
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+
 defineProps({
     headerData: Array,
     bodyData: Object || Array,
+    type: String,
 });
 </script>
 
@@ -10,7 +14,9 @@ defineProps({
         <thead>
             <tr>
                 <template v-for="hData in headerData" :key="hData">
-                    <th class="text-sm font-semibold text-gray-700 p-4 bg-gray-100">
+                    <th
+                        class="text-sm font-semibold text-gray-700 p-4 bg-gray-100"
+                    >
                         {{ hData }}
                     </th>
                 </template>
@@ -27,16 +33,103 @@ defineProps({
                             {{ bData.email }}
                         </td>
                         <td class="p-4 border-t border-gray-200">
-                            <a
-                                href="#"
-                                class="text-xs font-semibold text-indigo-600 hover:text-indigo-800"
-                                >Edit</a
+                            <template
+                                v-if="
+                                    bData.email != 'go.sedekah0711@gmail.com' &&
+                                    bData.email != $page.props.user.email
+                                "
                             >
-                            <a
-                                href="#"
-                                class="text-xs font-semibold text-red-600 hover:text-red-800 ml-4"
-                                >Delete</a
-                            >
+                                <Menu>
+                                    <MenuButton>
+                                        <EllipsisVerticalIcon
+                                            class="h-5 w-5 cursor-pointer"
+                                        />
+                                    </MenuButton>
+                                    <transition
+                                        enter-active-class="transition duration-100 ease-out"
+                                        enter-from-class="transform scale-95 opacity-0"
+                                        enter-to-class="transform scale-100 opacity-100"
+                                        leave-active-class="transition duration-75 ease-in"
+                                        leave-from-class="transform scale-100 opacity-100"
+                                        leave-to-class="transform scale-95 opacity-0"
+                                    >
+                                        <MenuItems
+                                            class="sm:absolute z-40 mt-1 origin-top-left divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                        >
+                                            <template v-if="type == 'admin'">
+                                                <div class="px-1 py-1">
+                                                    <MenuItem
+                                                        v-slot="{ active }"
+                                                    >
+                                                        <button
+                                                            :class="[
+                                                                active
+                                                                    ? 'bg-indigo-500 text-white'
+                                                                    : 'text-gray-900',
+                                                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                                            ]"
+                                                        >
+                                                            Remove from admin
+                                                        </button>
+                                                    </MenuItem>
+                                                </div>
+                                            </template>
+                                            <template
+                                                v-else-if="type == 'donor'"
+                                            >
+                                                <div class="px-1 py-1">
+                                                    <MenuItem
+                                                        v-slot="{ active }"
+                                                    >
+                                                        <button
+                                                            :class="[
+                                                                active
+                                                                    ? 'bg-indigo-500 text-white'
+                                                                    : 'text-gray-900',
+                                                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                                            ]"
+                                                        >
+                                                            Approve to admin
+                                                        </button>
+                                                    </MenuItem>
+                                                    <MenuItem
+                                                        v-slot="{ active }"
+                                                    >
+                                                        <button
+                                                            :class="[
+                                                                active
+                                                                    ? 'bg-indigo-500 text-white'
+                                                                    : 'text-gray-900',
+                                                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                                            ]"
+                                                        >
+                                                            Approve to needy
+                                                        </button>
+                                                    </MenuItem>
+                                                </div>
+                                            </template>
+                                            <template v-else>
+                                                <div class="px-1 py-1">
+                                                    <MenuItem
+                                                        v-slot="{ active }"
+                                                    >
+                                                        <button
+                                                            :class="[
+                                                                active
+                                                                    ? 'bg-indigo-500 text-white'
+                                                                    : 'text-gray-900',
+                                                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                                            ]"
+                                                        >
+                                                            Remove from needy
+                                                        </button>
+                                                    </MenuItem>
+                                                </div>
+                                            </template>
+                                        </MenuItems>
+                                    </transition>
+                                </Menu>
+                            </template>
                         </td>
                     </tr>
                 </template>
