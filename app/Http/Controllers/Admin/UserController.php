8580c3226115fe034a41dfe037081623fe2a5f97
Amril Azim <amrilazim@Amrils-MacBook-Pro.local>
2023-01-04 23:39:33 +0800

    create method for handle needy role assignment

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
             ->role(request('role'))
             ->limit(10)
             ->get();
-        
+
         return Inertia::render('Admin/Users', [
             'usersData' => [
                 'admins' => $admins,
@@ -29,6 +29,39 @@ class UserController extends Controller
             ],
             'filteredData' => $filteredData,
             'requestedData' => request(['name', 'role']),
-        ]);
+        ])
+            ->with('jetstream.flash.banner', session()->get('jetstream.flash.banner'))
+            ->with('jetstream.flash.bannerStyle', session()->get('jetstream.flash.bannerStyle'));
+    }
+
+    public function assignNeedyRole(User $user, Request $request)
+    {
+        // check if user has needy role
+        $needyValidate = User::role('needy')->where('id', $user->id)->exists();
+
+        // check if user has admin role
+        $adminValidate = User::role('admin')->where('id', $user->id)->exists();
+
+        if ($needyValidate) {
+            $request->session()->flash('jetstream.flash.banner', 'User already has needy role.');
+            $request->session()->flash('jetstream.flash.bannerStyle', 'danger');
+
+            return redirect()->route('admin.users');
+        }
+
+        if ($adminValidate) {
+            $request->session()->flash('jetstream.flash.banner', 'User is an admin, cannot assigned to this role.');
+            $request->session()->flash('jetstream.flash.bannerStyle', 'danger');
+
+            return redirect()->route('admin.users');
+        }
+
+        // perform database insertion.
+        $user->assignRole('needy');
+
+        $request->session()->flash('jetstream.flash.banner', 'Needy role successfully assigned!');
+        $request->session()->flash('jetstream.flash.bannerStyle', 'success');
+
+        return redirect()->route('admin.users');
     }
 }
