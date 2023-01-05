091ab31a6c85cc133cc86a4c9a1c0bdec7bd600b
Amril Azim <amrilazim@Amrils-MacBook-Pro.local>
2023-01-05 11:57:00 +0800

    register linkActive as props and use it to show the current active link

--- resources/js/Components/DropdownLink.vue
+++ resources/js/Components/DropdownLink.vue
@@ -1,9 +1,20 @@
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3';
+import { computed } from 'vue';
 
-defineProps({
+const props = defineProps({
     href: String,
     as: String,
+    linkActive: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const linkClasses = computed(() => {
+    return props.linkActive
+        ? 'block px-4 py-2 text-sm leading-5 bg-indigo-500 text-white hover:text-gray-700 hover:bg-indigo-400 focus:outline-none focus:bg-indigo-400 transition'
+        : 'block px-4 py-2 text-sm leading-5 text-gray-400 hover:text-white hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500 transition';
 });
 </script>
 
@@ -17,7 +28,7 @@ defineProps({
             <slot />
         </a>
 
-        <Link v-else :href="href" class="block px-4 py-2 text-sm leading-5 text-gray-400 hover:text-white hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500 transition">
+        <Link v-else :href="href" :class="linkClasses">
             <slot />
         </Link>
     </div>
