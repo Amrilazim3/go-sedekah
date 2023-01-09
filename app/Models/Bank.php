1e9f762ae8d72bedb0fab89517c0a05e86ebbdc7
Amril Azim <a.azim0711@gmail.com>
2023-01-09 16:16:06 +0800

    define one to one relationship with BankDetail model

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -10,4 +10,9 @@ class Bank extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function BankDetail()
+    {
+        return $this->hasOne(BankDetail::class, 'id', 'bank_detail_id');
+    }
 }
