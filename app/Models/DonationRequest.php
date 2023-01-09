cf51996fc4cab58f9ae730ac772aac9a2fb3476d
Amril Azim <a.azim0711@gmail.com>
2023-01-09 16:34:51 +0800

    define inverse one-to-one relationship method

--- app/Models/DonationRequest.php
+++ app/Models/DonationRequest.php
@@ -10,4 +10,14 @@ class DonationRequest extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function bank()
+    {
+        return $this->belongsTo(Bank::class, 'bank_id', 'id');
+    }
 }
