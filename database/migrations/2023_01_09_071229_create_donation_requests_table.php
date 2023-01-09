27afc9e195c46f9902b507e7e3c08316fec84f1a
Amril Azim <a.azim0711@gmail.com>
2023-01-09 15:52:12 +0800

    create table donation_requests

---
+++ database/migrations/2023_01_09_071229_create_donation_requests_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donation_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('bank_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('details');
+            $table->float('currently_received')->default(0);
+            $table->float('target_amount');
+            $table->string('status')->comment('pending / approved / rejected')->default('pending');
+            $table->boolean('is_verified')->default(false);
+            $table->timestamp('verification_expirty_at')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donation_requests');
+    }
+};
