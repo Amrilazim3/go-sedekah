38b3d06ba9fda6c27b875b5419d35c335038067d
Amril Azim <a.azim0711@gmail.com>
2023-01-11 07:45:41 +0800

    create custom select input component

---
+++ resources/js/Components/SelectInput.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    modelValue: String,
+    data: Array,
+});
+
+defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+    <select
+        class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+    >
+        <template v-for="bank in data" :key="bank">
+            <option :value="bank[0]">{{ bank[1] }}</option>
+        </template>
+    </select>
+</template>
