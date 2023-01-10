0e339da9e0b985778f0d5b831f7dd4231e0d91ea
Amril Azim <a.azim0711@gmail.com>
2023-01-10 12:08:47 +0800

    register billplz in providers's array and aliases's array

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
          * Package Service Providers...
          */
         Spatie\Permission\PermissionServiceProvider::class,
+        Billplz\Laravel\BillplzServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -213,6 +214,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Billplz' => Billplz\Laravel\Billplz::class,
     ])->toArray(),
 
 ];
