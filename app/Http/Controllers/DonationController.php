8b91ccead8e623d250854ee032c0a873b02f0137
Amril Azim <a.azim0711@gmail.com>
2023-01-13 18:34:01 +0800

    add index method

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -9,6 +9,10 @@ class DonationController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Donations');
+        return Inertia::render('Donations', [
+            'histories' => 'for all role',
+            'requests' => 'for admin role and needy role',
+            'users' => 'for admin to see all users donation histories',
+        ]);
     }
 }
