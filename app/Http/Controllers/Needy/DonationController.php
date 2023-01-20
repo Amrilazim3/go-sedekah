8ea5d1e8116be3fd908aeee6121dfa338be9579e
Amril Azim <a.azim0711@gmail.com>
2023-01-20 23:50:52 +0800

    create DonationController class

---
+++ app/Http/Controllers/Needy/DonationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Needy;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DonationController extends Controller
+{
+    public function index(Request $request)
+    {
+    }
+}
