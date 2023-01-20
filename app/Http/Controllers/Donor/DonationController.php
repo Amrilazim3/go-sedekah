ead7d5c4092a4ee22289cf52ea44600bd6eba4b9
Amril Azim <a.azim0711@gmail.com>
2023-01-20 23:51:38 +0800

    initialize Donor folder and create DonationController class

---
+++ app/Http/Controllers/Donor/DonationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Donor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DonationController extends Controller
+{
+    public function index(Request $request)
+    {
+        
+    }
+}
