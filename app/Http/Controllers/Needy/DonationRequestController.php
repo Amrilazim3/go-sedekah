9e0518bbf91e17353dc6d3c23f6f05445b8e3ff4
Amril Azim <a.azim0711@gmail.com>
2023-01-21 16:22:58 +0800

    create index method that return user's donation requests history

--- app/Http/Controllers/Needy/DonationRequestController.php
+++ app/Http/Controllers/Needy/DonationRequestController.php
@@ -9,6 +9,19 @@ use Illuminate\Http\Request;
 
 class DonationRequestController extends Controller
 {
+    public function index($user)
+    {
+        return DonationRequest::select([
+            'id',
+            'title',
+            'currently_received',
+            'target_amount',
+            'status',
+            'is_verified',
+            'created_at',
+            'verification_expiry_at'
+        ])->where('user_id', $user->id)->paginate(13, ['*'], 'requests');
+    }
     
     public function store(Request $request)
     {
