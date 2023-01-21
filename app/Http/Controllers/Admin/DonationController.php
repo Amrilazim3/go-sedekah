2bfac63adb00409dea4082acb7a220ea9e6277c8
Amril Azim <a.azim0711@gmail.com>
2023-01-22 02:25:41 +0800

    refactoring class

--- app/Http/Controllers/Admin/DonationController.php
+++ app/Http/Controllers/Admin/DonationController.php
@@ -9,34 +9,68 @@ use Illuminate\Http\Request;
 
 class DonationController extends Controller
 {
-    public function index(Request $request, $user)
+    const DONATION_COLUMN = [
+        'id',
+        'user_id',
+        'donation_request_id',
+        'amount',
+        'created_at'
+    ];
+
+    const DONATION_REQUEST_COLUMN = [
+        'id',
+        'user_id',
+        'title',
+        'currently_received',
+        'target_amount'
+    ];
+
+    const USER_COLUMN = ['id', 'name'];
+
+    public function index(Request $request)
     {
         // all user donation histories
-        $usersData = Donation::select([
-            'id',
-            'user_id',
-            'donation_request_id',
-            'amount',
-            'created_at'
-        ])->with(['donationRequest' => function ($query) {
-            return $query->select([
-                'id',
-                'user_id',
-                'currently_received',
-                'target_amount',
-                'title'
-            ])->with(['user' => function ($query) {
-                return $query->select(['id', 'name']);
-            }]);
-        }])->with(['user' => function ($query) {
-            return $query->select(['id', 'name']);
-        }])
+        $usersData = Donation::select(self::DONATION_COLUMN)
+            ->with(['donationRequest' => function ($query) {
+                return $query->select(self::DONATION_REQUEST_COLUMN)
+                    ->with(['user' => function ($query) {
+                        return $query->select(self::USER_COLUMN);
+                    }]);
+            }])->with(['user' => function ($query) {
+                return $query->select(self::USER_COLUMN);
+            }])
             ->paginate(13, ['*'], 'users');
 
         // all user donation requests
-        $donationRequestController = new DonationRequestController; 
-        $requestsData = $donationRequestController->index();
+        $requestsData = (new DonationRequestController)->index();
 
         return compact("usersData", "requestsData");
+    }
+
+    public function querySearchFilter(string $search)
+    {
+        return Donation::select(self::DONATION_COLUMN)
+            ->with(['donationRequest' => function ($query) {
+                return $query->select(self::DONATION_REQUEST_COLUMN)
+                    ->with(['user' => function ($query) {
+                        return $query->select(self::USER_COLUMN);
+                    }]);
+            }])
+            ->with(['user' => function ($query) {
+                return $query->select(self::USER_COLUMN);
+            }])
+            ->whereHas('user', function ($query) use ($search) {
+                $query->where('name', 'like', '%' . $search . '%');
+            })
+            ->orWhere(function ($query) use ($search) {
+                $query->whereHas('donationRequest', function ($query) use ($search) {
+                    $query->where('title', 'like', '%' . $search . '%');
+                })
+                    ->orWhereHas('donationRequest.user', function ($query) use ($search) {
+                        $query->where('name', 'like', '%' . $search . '%');
+                    });
+            })
+            ->limit(10)
+            ->get();
     }
 }
