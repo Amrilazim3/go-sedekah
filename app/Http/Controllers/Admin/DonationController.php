aaee1467e27fb116d480092cd4f3d6fb16554e32
Amril Azim <a.azim0711@gmail.com>
2023-01-21 11:27:28 +0800

    adding query to fetch only necessary data for admin

--- app/Http/Controllers/Admin/DonationController.php
+++ app/Http/Controllers/Admin/DonationController.php
@@ -3,12 +3,53 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Donation;
+use App\Models\DonationRequest;
 use Illuminate\Http\Request;
 
 class DonationController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, $user)
     {
-        
+        // all user donation histories
+        $usersData = Donation::select([
+            'id',
+            'user_id',
+            'donation_request_id',
+            'amount',
+            'created_at'
+        ])->with(['donationRequest' => function ($query) {
+            return $query->select([
+                'id',
+                'user_id',
+                'currently_received',
+                'target_amount',
+                'title'
+            ])->with(['user' => function ($query) {
+                return $query->select(['id', 'name']);
+            }]);
+        }])->with(['user' => function ($query) {
+            return $query->select(['id', 'name']);
+        }])
+            ->paginate(13, ['*'], 'users');
+
+        // all user donation requests
+        $requestsData = DonationRequest::select([
+            'id',
+            'user_id',
+            'title',
+            'detail',
+            'currently_received',
+            'target_amount',
+            'status',
+            'is_verified',
+            'created_at',
+            'verification_expiry_at'
+        ])->with(['user' => function ($query) {
+            return $query->select(['id', 'name']);
+        }])
+            ->paginate(13, ['*'], 'requests');
+
+        return compact("usersData", "requestsData");
     }
 }
