91002fffd3e726a0c53c7e130f8b2305c139b8f5
Amril Azim <a.azim0711@gmail.com>
2023-01-21 11:28:05 +0800

    adding query to fetch only necessary data for Donor

--- app/Http/Controllers/Needy/DonationController.php
+++ app/Http/Controllers/Needy/DonationController.php
@@ -3,11 +3,39 @@
 namespace App\Http\Controllers\Needy;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bank;
+use App\Models\DonationRequest;
 use Illuminate\Http\Request;
 
 class DonationController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, $user)
     {
+        // fetch bank account here $bankAccounts here
+        $bankAccounts = [];
+
+        $bankAccountsArr = Bank::select([
+            'id',
+            'user_id',
+            'account_number'
+        ])->where('user_id', $user->id)->get();
+
+        foreach ($bankAccountsArr as $bank) {
+            $bankAccounts[] = [$bank['id'], $bank['account_number']];
+        }
+
+        // donation requests 
+        $requestsData = DonationRequest::select([
+            'id',
+            'title',
+            'currently_received',
+            'target_amount',
+            'status',
+            'is_verified',
+            'created_at',
+            'verification_expiry_at'
+        ])->where('user_id', $user->id)->paginate(13, ['*'], 'requests');
+
+        return compact("bankAccounts", "requestsData");
     }
 }
