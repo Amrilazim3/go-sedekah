f7affcaa1e370b85145ddadb6b70b93e068a53cd
Amril Azim <a.azim0711@gmail.com>
2023-01-21 11:49:41 +0800

    update route name and the associate class controller

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DonationController;
-use App\Http\Controllers\Needy\BankController as NeedyBankController;
+use App\Http\Controllers\Needy\BankController;
 use App\Http\Controllers\WelcomeController;
 use Billplz\Laravel\Billplz;
 use GuzzleHttp\Client;
@@ -33,11 +33,25 @@ Route::middleware([
 ])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
-    Route::get('/donations', [DonationController::class, 'index'])->name('donations.index');
-    Route::post('/donations', [DonationController::class, 'store'])->name('donations.store');
-    Route::delete('/donations/{donationRequest}', [DonationController::class, 'destroy'])->name('donations.destroy');
-    Route::patch('/donations/{donationRequest}/approve', [DonationController::class, 'approveRequest'])->name('donations.approveRequest');
-    Route::patch('/donations/{donationRequest}/reject', [DonationController::class, 'rejectRequest'])->name('donations.rejectRequest');
+    Route::get('/donations', [
+        App\Http\Controllers\DonationController::class,
+        'index'
+    ])->name('donations.index');
+
+    Route::post('/donation-request', [
+        App\Http\Controllers\Needy\DonationRequestController::class,
+        'store'
+    ])->name('donationRequest.store');
+    Route::delete('/donation-request/{donationRequest}', [
+        App\Http\Controllers\Needy\DonationRequestController::class,
+        'destroy'
+    ])->name('donationRequest.destroy');
+    Route::patch('/donation-request/{donationRequest}/approve', [
+        App\Http\Controllers\Admin\DonationRequestController::class, 'approve'
+    ])->name('donationRequest.approve');
+    Route::patch('/donation-request/{donationRequest}/reject', [
+        App\Http\Controllers\Admin\DonationRequestController::class, 'reject'
+    ])->name('donationRequest.reject');
 
     Route::post('/billplz', function (Request $request) {
         // $bill = Billplz::bill()->create('bgs0cicq', 'amril@gmail.com', '0172374050', 'amril azim', 23000, 'http://localhost', 'donation for fulan bin fulan', ['redirect_url' => 'http://localhost']);
@@ -58,7 +72,7 @@ Route::middleware([
 
         // get open collection 
         // dd(Http::withBasicAuth('fe3b1a4d-889f-404e-9fcf-59f2314e83a1:', '')->get('https://www.billplz-sandbox.com/api/v3/open_collections/b_xg5irk')->json());
-        
+
         // get bill
         // dd(Http::withBasicAuth('fe3b1a4d-889f-404e-9fcf-59f2314e83a1:', '')->get('https://www.billplz-sandbox.com/api/v3/bills/o3ieijra')->json());
     });
@@ -90,9 +104,8 @@ Route::middleware([
         ->prefix('needy')
         ->name('needy.')
         ->group(function () {
-            Route::get('/banks', [NeedyBankController::class, 'index'])->name('banks.index');
-            Route::post('/banks', [NeedyBankController::class, 'store'])->name('banks.store');
-            Route::delete('/banks/{bank}', [NeedyBankController::class, 'destroy'])->name('banks.destroy');
+            Route::get('/banks', [BankController::class, 'index'])->name('banks.index');
+            Route::post('/banks', [BankController::class, 'store'])->name('banks.store');
+            Route::delete('/banks/{bank}', [BankController::class, 'destroy'])->name('banks.destroy');
         });
-
 });
