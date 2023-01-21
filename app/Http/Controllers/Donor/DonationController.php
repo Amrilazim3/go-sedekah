72a4e675850abbc8d6e1ee14c4bcfd3d26ceb938
Amril Azim <a.azim0711@gmail.com>
2023-01-21 11:27:56 +0800

    adding query to fetch only necessary data for Donor

--- app/Http/Controllers/Donor/DonationController.php
+++ app/Http/Controllers/Donor/DonationController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers\Donor;
 
 use App\Http\Controllers\Controller;
+use App\Models\Donation;
 use Illuminate\Http\Request;
 
 class DonationController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, $user)
     {
-        
+        $historiesData = Donation::select([
+            'id',
+            'user_id',
+            'donation_request_id',
+            'amount',
+            'bill_url',
+            'created_at'
+        ])->with(['donationRequest' => function ($query) {
+            return $query->select([
+                'id',
+                'user_id',
+                'currently_received',
+                'target_amount',
+                'title'
+            ])->with(['user' => function ($query) {
+                return $query->select(['id', 'name']);
+            }]);
+        }])->where('user_id', $user->id)->paginate(13, ['*'], 'histories');
+
+        return compact("historiesData");
     }
 }
