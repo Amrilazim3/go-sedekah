db6915de0588e7d583676c72fd79f1b7bb3fb056
Amril Azim <a.azim0711@gmail.com>
2023-01-21 16:23:29 +0800

    call other class in index class

--- app/Http/Controllers/Needy/DonationController.php
+++ app/Http/Controllers/Needy/DonationController.php
@@ -11,30 +11,13 @@ class DonationController extends Controller
 {
     public function index(Request $request, $user)
     {
-        // fetch bank account here $bankAccounts here
-        $bankAccounts = [];
+        // fetch bank account
+        $bankController = new BankController;
+        $bankAccounts = $bankController->getIdAndAccountNumber($user);
 
-        $bankAccountsArr = Bank::select([
-            'id',
-            'user_id',
-            'account_number'
-        ])->where('user_id', $user->id)->get();
-
-        foreach ($bankAccountsArr as $bank) {
-            $bankAccounts[] = [$bank['id'], $bank['account_number']];
-        }
-
-        // donation requests 
-        $requestsData = DonationRequest::select([
-            'id',
-            'title',
-            'currently_received',
-            'target_amount',
-            'status',
-            'is_verified',
-            'created_at',
-            'verification_expiry_at'
-        ])->where('user_id', $user->id)->paginate(13, ['*'], 'requests');
+        // donation requests
+        $donationRequestController = new DonationRequestController;
+        $requestsData = $donationRequestController->index($user);
 
         return compact("bankAccounts", "requestsData");
     }
