29ea1908b8a52a517523ec9cadd6b8eba17e73b5
Amril Azim <a.azim0711@gmail.com>
2023-01-24 07:59:57 +0800

    create nofication class for needy after to send email after success creating donation request

---
+++ app/Notifications/Needy/Donation/RequestSent.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications\Needy\Donation;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class RequestSent extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->greeting('Hye ' . $notifiable->name)
+            ->subject('New Donation Request')
+            ->line('The user called ' . $this->user->name . '(' . $this->user->email . ')' . ' make a donation request.')
+            ->action('View Now', url('/donations'))
+            ->line('Hope you can review the request as soon as possible!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
