5f738f5cb0373038fedcc1a48b76758c6140f2a2
Amril Azim <a.azim0711@gmail.com>
2023-01-23 17:52:26 +0800

    create DonationTest class

---
+++ tests/Feature/DonationTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DonationTest extends TestCase
+{
+    public function test_donations_page_can_be_rendered()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $response = $this->get('/donations');
+
+        $response->assertSeeInOrder([
+            'props',
+            'dataRoles',
+            'queryResult',
+            'queryParams'
+        ], false);
+
+        $response->assertOk();
+    }
+}
