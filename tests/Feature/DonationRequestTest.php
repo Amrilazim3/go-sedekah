63295d7304fab70b9ad4c4dc545f0d0e256da357
Amril Azim <a.azim0711@gmail.com>
2023-01-24 17:31:14 +0800

    add test method to check if needy user can delete donation request

--- tests/Feature/DonationRequestTest.php
+++ tests/Feature/DonationRequestTest.php
@@ -4,12 +4,16 @@ namespace Tests\Feature;
 
 use App\Models\Bank;
 use App\Models\BankDetail;
+use App\Models\DonationRequest;
 use App\Models\User;
+use App\Notifications\Needy\Donation\RequestDeleted;
 use App\Notifications\Needy\Donation\RequestSent;
 use Database\Seeders\BankDetailSeeder;
+use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Queue;
 use Spatie\Permission\Models\Role;
 use Tests\TestCase;
 
@@ -20,27 +24,25 @@ class DonationRequestTest extends TestCase
     public function test_needy_user_can_make_donation_request()
     {
         Bus::fake();
+        // Queue::fake();
 
-        $this->seed(BankDetailSeeder::class);
-        $bankDetail = BankDetail::first();
-
-        $roles = ['needy', 'admin'];
-        foreach ($roles as $role) {
-            Role::create([
-                'name' => $role
-            ]);
-        }
+        $this->seed([
+            BankDetailSeeder::class,
+            RoleSeeder::class
+        ]);
 
         $this->actingAs($user = User::factory()->create());
 
         $user->assignRole('needy');
 
+        $bankDetail = BankDetail::first();
+
         $bank = Bank::create([
             'user_id' => $user->id,
             'bank_detail_id' => $bankDetail->id,
             'name_on_card' => fake()->name(),
-            'ic_number' => '030307010711',
-            'account_number' => '151584282718',
+            'ic_number' => fake()->numberBetween(pow(10, 11), pow(10,12) - 1),
+            'account_number' => fake()->creditCardNumber(null, false, ''),
         ]);
 
         $response = $this->post('/needy/donation-request', [
@@ -49,6 +51,50 @@ class DonationRequestTest extends TestCase
             'detail' => fake()->paragraph(1),
             'targetAmount' => rand(10, 100)
         ]);
+
+        // Queue::assertPushed(RequestSent::class);
+
+        // Bus::dispatch(new RequestDeleted);
+        // Bus::assertDispatched(RequestSent::class);
+
+        $response->assertRedirectToRoute('donations.index');
+    }
+
+    public function test_needy_user_can_delete_donation_request()
+    {
+        Bus::fake();
+
+        $this->seed([
+            BankDetailSeeder::class,
+            RoleSeeder::class
+        ]);
+
+        $this->actingAs($user = User::factory()->create());
+
+        $user->assignRole('needy');
+
+        $bankDetail = BankDetail::first();
+
+        $bank = Bank::create([
+            'user_id' => $user->id,
+            'bank_detail_id' => $bankDetail->id,
+            'name_on_card' => fake()->name(),
+            'ic_number' => fake()->numberBetween(pow(10, 11), pow(10,12) - 1),
+            'account_number' => fake()->creditCardNumber(null, false, ''),
+        ]);
+
+        $donationRequest = DonationRequest::create([
+            'user_id' => $user->id,
+            'bank_id' => $bank->id,
+            'title' => fake()->text(30),
+            'detail' => fake()->paragraph(1),
+            'currently_received' => 0,
+            'target_amount' => rand(10, 100),
+        ]);
+
+        $response = $this->delete('/needy/donation-request/' . $donationRequest->id);
+
+        $this->assertModelMissing($donationRequest);
 
         Bus::dispatch(new RequestSent($user));
 
