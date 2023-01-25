b102d393a20be4ae04da413197e5ac3bdb408987
Amril Azim <a.azim0711@gmail.com>
2023-01-25 15:46:05 +0800

    add notification for success assign needy's role

---
+++ app/Notifications/Admin/Role/NeedyRoleAdded.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications\Admin\Role;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NeedyRoleAdded extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->greeting('Hye ' . $notifiable->name)
+            ->line('We are glad to say that your account now has a \'needy\' role, which is can make a donation request in this application!')
+            ->action('create donation request now', url('/donations'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
