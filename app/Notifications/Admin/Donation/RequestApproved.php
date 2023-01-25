5b234c454e282ce6b9db7fd1051a58d05edc4f4c
Amril Azim <a.azim0711@gmail.com>
2023-01-25 10:48:44 +0800

    set donation request model instance as variable

--- app/Notifications/Admin/Donation/RequestApproved.php
+++ app/Notifications/Admin/Donation/RequestApproved.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications\Admin\Donation;
 
+use App\Models\DonationRequest;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -11,14 +12,16 @@ class RequestApproved extends Notification implements ShouldQueue
 {
     use Queueable;
 
+    public $donationRequest;
+
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(DonationRequest $donationRequest)
     {
-        //
+        $this->donationRequest = $donationRequest;
     }
 
     /**
@@ -41,9 +44,11 @@ class RequestApproved extends Notification implements ShouldQueue
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+            ->greeting('Hye ' . $notifiable->name)
+            ->line('Your donation request that you made at ' . $this->donationRequest->created_at . ' has been approved by our admin')
+            ->line('You donation request now can accept a donation from others.')
+            ->action('You can view it here', url('/donations'))
+            ->line('Thank you for using our application!');
     }
 
     /**
