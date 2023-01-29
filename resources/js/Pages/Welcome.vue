6fe095081e66c54468f718d8125daeca762fe769
Amril Azim <a.azim0711@gmail.com>
2023-01-29 22:13:24 +0800

    add donation form

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,10 +1,25 @@
 <script setup>
-import { Head, Link } from "@inertiajs/inertia-vue3";
+import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
 import Footer from "@/Components/Footer.vue";
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import PaginationBar from "@/Components/PaginationBar.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import TextInput from "@/Components/TextInput.vue";
+import TextAreaInput from "@/Components/TextAreaInput.vue";
+import SelectInput from "@/Components/SelectInput.vue";
+import Checkbox from "@/Components/Checkbox.vue";
+import {
+    TransitionRoot,
+    TransitionChild,
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+} from "@headlessui/vue";
+import { inject } from "vue";
+import { usePage } from '@inertiajs/inertia-vue3';
 
 const props = defineProps({
     canLogin: Boolean,
@@ -12,9 +27,56 @@ const props = defineProps({
     donationRequestsData: Object,
 });
 
+const Swal = inject("$swal");
+
 const donationRequests = computed(() => {
-    return props.donationRequestsData.data;
+    return props.donationRequestsData;
 });
+const isOpenDonationModel = ref(false);
+const currentOpenDonationId = ref(0);
+const donationForm = useForm({
+    name: "",
+    email: "",
+    amount: "",
+    message: "",
+    isHidden: false,
+});
+
+const openDonationModel = (id) => {
+    isOpenDonationModel.value = true;
+    currentOpenDonationId.value = id;
+};
+
+const closeDonationModel = () => {
+    isOpenDonationModel.value = false;
+    currentOpenDonationId.value = 0;
+};
+
+const donate = () => {
+    donationForm
+        .transform((data) => ({
+            ...data,
+        }))
+        .post(`/donations/${currentOpenDonationId.value}`, {
+            preserveScroll: true,
+            onSuccess: () => {
+                Swal.fire({
+                    title: "Redirecting to payment page",
+                    html: "please wait for a moment...",
+                    didOpen: () => {
+                        Swal.showLoading();
+                    },
+                    allowOutsideClick: false,
+                    allowEscapeKey: false,
+                    allowEnterKey: false,
+                });
+
+                const billUrl = usePage().props.value.jetstream.flash?.billplzID;
+
+                window.location.href = billUrl;
+            },
+        });
+};
 </script>
 
 <template>
@@ -163,9 +225,14 @@ const donationRequests = computed(() => {
                         <div
                             class="bg-gray-200 rounded-lg shadow-md p-4 md:grid md:grid-cols-3"
                         >
-                            <template v-if="donationRequests.length > 0">
-                                <template v-for="request in donationRequests" :key="request.id">
-                                    <div class="mb-4 md:col-span-1 md:pr-3 lg:pr-5">
+                            <template v-if="donationRequests.data.length > 0">
+                                <template
+                                    v-for="request in donationRequests.data"
+                                    :key="request.id"
+                                >
+                                    <div
+                                        class="mb-4 md:col-span-1 md:pr-3 lg:pr-5"
+                                    >
                                         <h3 class="text-lg font-medium">
                                             {{ request.title }}
                                             <span
@@ -176,24 +243,43 @@ const donationRequests = computed(() => {
                                         <p class="text-gray-600 mt-1">
                                             {{ request.detail }}
                                         </p>
-                                        <div class="mt-3 flex justify-end space-x-3">
-                                            <div class="self-end text-xs text-gray-600">
-                                                <span class="text-indigo-500">{{ request.progress }}</span>
-                                                ({{ request.currently_received }} MYR / {{ request.target_amount }} MYR)
+                                        <div
+                                            class="mt-3 flex justify-end space-x-3"
+                                        >
+                                            <div
+                                                class="self-end text-xs text-gray-600"
+                                            >
+                                                <span class="text-indigo-500">{{
+                                                    request.progress
+                                                }}</span>
+                                                ({{
+                                                    request.currently_received
+                                                }}
+                                                MYR /
+                                                {{ request.target_amount }} MYR)
                                             </div>
                                             <button
                                                 class="bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600"
+                                                @click.prevent="
+                                                    openDonationModel(
+                                                        request.id
+                                                    )
+                                                "
                                             >
                                                 Donate
                                             </button>
                                         </div>
                                     </div>
                                 </template>
+                                <PaginationBar
+                                    :links="donationRequests.links"
+                                />
                             </template>
                             <template v-else>
-                                <p class="text-lg font-semibold">No donation requests yet...</p>
+                                <p class="text-lg font-semibold">
+                                    No donation requests yet...
+                                </p>
                             </template>
-                            <PaginationBar :links="donationRequests.links" />
                         </div>
                     </div>
                 </div>
@@ -202,4 +288,145 @@ const donationRequests = computed(() => {
 
         <Footer />
     </div>
+
+    <TransitionRoot appear :show="isOpenDonationModel" as="template">
+        <Dialog as="div" @close="closeDonationModel" class="relative z-10">
+            <TransitionChild
+                as="template"
+                enter="duration-300 ease-out"
+                enter-from="opacity-0"
+                enter-to="opacity-100"
+                leave="duration-200 ease-in"
+                leave-from="opacity-100"
+                leave-to="opacity-0"
+            >
+                <div class="fixed inset-0 bg-black bg-opacity-25" />
+            </TransitionChild>
+
+            <div class="fixed inset-0 overflow-y-auto">
+                <div
+                    class="flex min-h-full items-center justify-center p-4 text-center"
+                >
+                    <TransitionChild
+                        as="template"
+                        enter="duration-300 ease-out"
+                        enter-from="opacity-0 scale-95"
+                        enter-to="opacity-100 scale-100"
+                        leave="duration-200 ease-in"
+                        leave-from="opacity-100 scale-100"
+                        leave-to="opacity-0 scale-95"
+                    >
+                        <DialogPanel
+                            class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+                        >
+                            <form @submit.prevent="donate()">
+                                <DialogTitle
+                                    as="h3"
+                                    class="text-lg font-medium leading-6 text-gray-900"
+                                >
+                                    Donate form
+                                </DialogTitle>
+                                <div class="mt-4">
+                                    <InputLabel for="name" value="Name" />
+                                    <TextInput
+                                        id="name"
+                                        v-model="donationForm.name"
+                                        type="text"
+                                        class="mt-1 block w-full"
+                                        required
+                                        autofocus
+                                        placeholder="john doe"
+                                    />
+                                    <InputError
+                                        class="mt-2"
+                                        :message="donationForm.errors.name"
+                                    />
+                                </div>
+                                <div class="mt-4">
+                                    <InputLabel
+                                        for="email"
+                                        value="Email Address"
+                                    />
+                                    <TextInput
+                                        id="email"
+                                        v-model="donationForm.email"
+                                        type="email"
+                                        class="mt-1 block w-full"
+                                        required
+                                        placeholder="johndoe@gmail.com"
+                                    />
+                                    <InputError
+                                        class="mt-2"
+                                        :message="donationForm.errors.email"
+                                    />
+                                </div>
+                                <div class="mt-4">
+                                    <InputLabel for="amount" value="Amount" />
+                                    <TextInput
+                                        id="amount"
+                                        v-model="donationForm.amount"
+                                        type="number"
+                                        class="mt-1 block w-full"
+                                        required
+                                        placeholder="50"
+                                    />
+                                    <InputError
+                                        class="mt-2"
+                                        :message="donationForm.errors.amount"
+                                    />
+                                </div>
+                                <div class="mt-4">
+                                    <InputLabel for="message" value="Message" />
+                                    <TextAreaInput
+                                        id="message"
+                                        v-model="donationForm.message"
+                                        class="mt-1 block w-full"
+                                        required
+                                    />
+                                    <InputError
+                                        class="mt-2"
+                                        :message="donationForm.errors.message"
+                                    />
+                                </div>
+                                <div class="mt-4 flex">
+                                    <Checkbox
+                                        id="is-hidden"
+                                        v-model:checked="donationForm.isHidden"
+                                        class="self-center"
+                                    />
+
+                                    <div
+                                        class="ml-2 self-center text-sm text-gray-600"
+                                    >
+                                        hide the donator information (anymous)
+                                    </div>
+                                </div>
+                                <div class="flex justify-end space-x-1.5 mt-4">
+                                    <button
+                                        type="button"
+                                        class="inline-flex justify-center rounded-md border border-transparent bg-red-500 px-4 py-2 text-sm font-medium text-gray-800 hover:bg-red-400 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                                        @click="closeDonationModel"
+                                    >
+                                        Cancel
+                                    </button>
+                                    <button
+                                        type="submit"
+                                        class="inline-flex justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                        :class="
+                                            donationForm.processing
+                                                ? 'bg-indigo-100 cursor-not-allowed'
+                                                : 'bg-indigo-500 hover:bg-indigo-400'
+                                        "
+                                        :disabled="donationForm.processing"
+                                    >
+                                        Submit
+                                    </button>
+                                </div>
+                            </form>
+                        </DialogPanel>
+                    </TransitionChild>
+                </div>
+            </div>
+        </Dialog>
+    </TransitionRoot>
 </template>
