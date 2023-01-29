e9faffdf48494a86e0ab7460d934dfde8cbffca0
Amril Azim <a.azim0711@gmail.com>
2023-01-29 11:18:00 +0800

    tsst if user can give donation

--- tests/Feature/DonationTest.php
+++ tests/Feature/DonationTest.php
@@ -2,13 +2,18 @@
 
 namespace Tests\Feature;
 
+use App\Models\DonationRequest;
 use App\Models\User;
+use Database\Seeders\BankDetailSeeder;
+use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DonationTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_donations_screen_can_be_rendered()
     {
         $this->actingAs($user = User::factory()->create());
@@ -21,6 +26,28 @@ class DonationTest extends TestCase
             'queryResult',
             'queryParams'
         ], false);
+
+        $response->assertOk();
+    }
+
+    public function test_user_can_give_donation()
+    {
+        $donationRequest = DonationRequest::factory()->create();
+
+        $this->actingAs($user = User::factory()->create());
+
+        $response = $this->post('/donations/' . $donationRequest->id, [
+            'name' => $user->name,
+            'email' => $user->email,
+            'amount' => rand(10, 50),
+            'message' => fake()->paragraph(1),
+            'isHidden' => false,
+        ]);
+
+        $this->assertDatabaseHas('donations', [
+            'user_id' => $user->id,
+            'donation_request_id' => $donationRequest->id
+        ]);
 
         $response->assertOk();
     }
