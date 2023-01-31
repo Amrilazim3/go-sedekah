0a806375ecc6d4a0f72cf5e15431010d58588efd
Amril Azim <a.azim0711@gmail.com>
2023-01-31 11:28:26 +0800

    fixed typo

--- app/Notifications/Needy/Donation/VerificationExpiration.php
+++ app/Notifications/Needy/Donation/VerificationExpiration.php
@@ -47,7 +47,7 @@ class VerificationExpiration extends Notification implements ShouldQueue
             ->greeting('Hye ' . $notifiable->name)
             ->line('Your donation request with title (' . $this->donationRequest->title . ') has successfully reached donation target amount.')
             ->line('In order to ensure you are not a scammer that trying to take advantage of our application, 
-            you must verify your donation at ' . $this->donationRequest->verification_expiry_at . ' request by providing some 
+            you must verify your donation request at ' . $this->donationRequest->verification_expiry_at . ' by providing some 
             prove of your request (receipt, transaction history, etc)
             and send it through this email go.sedekah0711@gmail.com.'
             )
