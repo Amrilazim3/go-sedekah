2612e3efbc68e3738ff59d204da157ade224dd4f
Amril Azim <a.azim0711@gmail.com>
2023-01-31 15:45:44 +0800

    create notification for successful verified donation request

---
+++ app/Notifications/Admin/Donation/RequestVerified.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications\Admin\Donation;
+
+use App\Models\DonationRequest;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class RequestVerified extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $donationRequest;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(DonationRequest $donationRequest)
+    {
+        $this->donationRequest = $donationRequest;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->greeting('Hye ' . $notifiable->name)
+            ->line('Your donation request called "' . $this->donationRequest->title . '" has been verified by our admin.')
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
