909d67b9748857ab20a80c2d2f26f11343978ce7
Amril Azim <a.azim0711@gmail.com>
2023-02-04 09:02:22 +0800

    generate and write logic in DahsboardController class

---
+++ app/Http/Controllers/Donor/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Donor;
+
+use App\Http\Controllers\Controller;
+use App\Models\Donation;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(Request $request)
+    {
+        return [
+            'totalDonationAmount' => Donation::where('user_id', $request->user()->id)->sum('amount'),
+            'recentDonationAmount' => Donation::with(['donationRequest' => function ($query) {
+                $query->with(['user' => function ($query) {
+                    $query->select(['id', 'name']);
+                }]);
+            }])
+                ->where('user_id', $request->user()->id)
+                ->orderByDesc('created_at')
+                ->first()
+        ];
+    }
+}
