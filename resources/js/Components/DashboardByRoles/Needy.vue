a3fab68df8f6022748e6e3efe7a0db368ae6a7d4
Amril Azim <a.azim0711@gmail.com>
2023-02-04 12:20:02 +0800

    make code reflect with null data

--- resources/js/Components/DashboardByRoles/Needy.vue
+++ resources/js/Components/DashboardByRoles/Needy.vue
@@ -1,7 +1,7 @@
 <script setup>
 const props = defineProps({
-    data: Object
-})
+    data: Object,
+});
 </script>
 
 <template>
@@ -20,21 +20,25 @@ const props = defineProps({
                     <h3 class="text-md font-semibold text-indigo-500">
                         Donation received total :
                     </h3>
-                    <h3 class="text-md">$500.50</h3>
+                    <h3 class="text-md">
+                        {{ props.data.donationReceivedTotalAmount }} MYR
+                    </h3>
                 </div>
                 <div class="sm:flex-1">
                     <h3 class="text-md font-semibold text-indigo-500">
                         Donation request made total :
                     </h3>
-                    <h3 class="text-md">5 times</h3>
+                    <h3 class="text-md">
+                        {{ props.data.donationRequestsMade }} times
+                    </h3>
                 </div>
             </div>
             <div class="sm:flex">
                 <div class="mt-6 sm:-mt-0 sm:last:flex-1">
                     <h3 class="text-md font-semibold text-indigo-500">
-                        This month donation received :
+                        Total donator :
                     </h3>
-                    <p class="text-md">$20</p>
+                    <p class="text-md">{{ props.data.totalDonator }}</p>
                 </div>
             </div>
         </div>
@@ -49,67 +53,73 @@ const props = defineProps({
         <h3 class="text-lg text-gray-800 font-semibold mb-2">
             Recent Donation Request
         </h3>
-        <div class="relative rounded-md shadow-sm">
-            <div
-                class="px-4 py-5 bg-indigo-100 rounded-tr-md rounded-tl-md sm:p-6"
-            >
+        <template v-if="props.data.recentDonationRequest">
+            <div class="relative rounded-md shadow-sm">
                 <div
-                    class="flex justify-between text-md leading-5 font-medium text-gray-900"
+                    class="px-4 py-5 bg-indigo-100 rounded-tr-md rounded-tl-md sm:p-6"
                 >
-                    <h3>For my personal medecine</h3>
-                    <h3 class="text-gray-600 text-sm">5 November 2021</h3>
-                </div>
-                <div class="mt-2 sm:flex sm:justify-between">
-                    <div class="sm:flex">
-                        <div class="mr-6 text-sm leading-5 text-gray-500">
-                            <h4 class="text-gray-700">Details</h4>
-                            <p>
-                                My teeth last week broke becauase i eat to much
-                                crab without stop for 3 hours, right now my teeh
-                                is look old lady that need to be replace and the
-                                cost for replacement is so high
-                            </p>
+                    <div
+                        class="flex justify-between text-md leading-5 font-medium text-gray-900"
+                    >
+                        <h3>For my personal medecine</h3>
+                        <h3 class="text-gray-600 text-sm">5 November 2021</h3>
+                    </div>
+                    <div class="mt-2 sm:flex sm:justify-between">
+                        <div class="sm:flex">
+                            <div class="mr-6 text-sm leading-5 text-gray-500">
+                                <h4 class="text-gray-700">Details</h4>
+                                <p>
+                                    My teeth last week broke becauase i eat to
+                                    much crab without stop for 3 hours, right
+                                    now my teeh is look old lady that need to be
+                                    replace and the cost for replacement is so
+                                    high
+                                </p>
+                            </div>
+                        </div>
+                        <div
+                            class="mt-4 sm:mt-0 sm:ml-4 sm:flex-shrink-0 sm:flex sm:justify-end"
+                        >
+                            <dl>
+                                <dt class="text-sm text-gray-700">
+                                    Current Donation Received
+                                </dt>
+                                <dd
+                                    class="text-sm leading-5 font-medium text-blue-600"
+                                >
+                                    $50
+                                </dd>
+                            </dl>
+                            <dl class="sm:ml-4">
+                                <dt class="text-sm text-gray-700">
+                                    Donation Goals
+                                </dt>
+                                <dd
+                                    class="text-sm leading-5 font-medium text-green-600"
+                                >
+                                    $100
+                                </dd>
+                            </dl>
                         </div>
                     </div>
-                    <div
-                        class="mt-4 sm:mt-0 sm:ml-4 sm:flex-shrink-0 sm:flex sm:justify-end"
-                    >
-                        <dl>
-                            <dt class="text-sm text-gray-700">
-                                Current Donation Received
-                            </dt>
-                            <dd
-                                class="text-sm leading-5 font-medium text-blue-600"
-                            >
-                                $50
-                            </dd>
-                        </dl>
-                        <dl class="sm:ml-4">
-                            <dt class="text-sm text-gray-700">
-                                Donation Goals
-                            </dt>
-                            <dd
-                                class="text-sm leading-5 font-medium text-green-600"
-                            >
-                                $100
-                            </dd>
-                        </dl>
+                </div>
+                <div
+                    class="bg-gray-100 rounded-br-md rounded-bl-md px-4 py-4 sm:px-6"
+                >
+                    <div class="text-sm leading-5">
+                        <a
+                            href="#"
+                            class="font-medium text-indigo-600 hover:text-indigo-500 transition duration-150 ease-in-out"
+                        >
+                            View More
+                        </a>
                     </div>
                 </div>
             </div>
-            <div
-                class="bg-gray-100 rounded-br-md rounded-bl-md px-4 py-4 sm:px-6"
-            >
-                <div class="text-sm leading-5">
-                    <a
-                        href="#"
-                        class="font-medium text-indigo-600 hover:text-indigo-500 transition duration-150 ease-in-out"
-                    >
-                        View More
-                    </a>
-                </div>
-            </div>
-        </div>
+        </template>
+        <template v-else>
+            <h3>No request is made yet.</h3>
+        </template>
     </div>
 
     <div class="py-8">
@@ -119,9 +129,14 @@ const props = defineProps({
     <!-- Recent donator -->
     <div>
         <h3 class="text-lg text-gray-800 font-semibold">Recent Donator</h3>
-        <p class="text-gray-800">
-            <span class="text-indigo-500 font-semibold">Kamal Adli</span>
-            donated you $50
-        </p>
+        <template v-if="props.data.recentDonator">
+            <p class="text-gray-800">
+                <span class="text-indigo-500 font-semibold">{{
+                    props.data.recentDonator.user.name
+                }}</span>
+                donated you {{ props.data.recentDonator.amount }} MYR
+            </p>
+        </template>
+        <template v-else><h3>No recent donator.</h3></template>
     </div>
 </template>
